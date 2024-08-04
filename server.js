7c7a9723155dccab01f07090a85a9f0b6c385ae5
SubodhSingh8543 <subodhsingh8543@gmail.com>
2024-08-04 13:46:20 +0530

    socket

---
+++ server.js
@@ -0,0 +1,26 @@
+const WebSocket = require('ws');
+const data = require("./inputUpdates.json");
+
+const server = new WebSocket.Server({ port: 8080 });
+
+server.on('connection', socket => {
+    console.log('Client connected');
+
+    let orders = data;
+
+    const sendUpdates = (updates, delay) => {
+        setTimeout(() => {
+            updates.forEach(update => {
+                socket.send(JSON.stringify(update));
+                console.log(`Update sent to order book at ${new Date().toISOString()} for client ${update.ClientID} : ${JSON.stringify(update)}`)
+            });
+        }, delay);
+    };
+
+    sendUpdates(orders.slice(0, 10), 1000);
+    sendUpdates(orders.slice(10, 30), 3000);
+    sendUpdates(orders.slice(30, 70), 6000);
+    sendUpdates(orders.slice(70, 100), 11000);
+});
+
+console.log('WebSocket server started on port 8080');
